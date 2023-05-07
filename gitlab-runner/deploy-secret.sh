d933b437693d408849990a4330e1c31a85602819
slonnik <slonnik@gmail.com>
2023-05-07 14:24:49 +0000

    update

--- gitlab-runner/deploy-secret.sh
+++ gitlab-runner/deploy-secret.sh
@@ -2,4 +2,8 @@
 
 microk8s.kubectl create secret generic main-secret \
 	--namespace gitlab-runner \
-	--from-file=gitlab.slonnik.site.crt=/home/slonnik/ssl/slonnik.site.crt
+	--from-file=gitlab.slonnik.site.crt=/home/slonnik/ssl/slonnik.site.crt \
+	--from-file=gitlab.slonnik.site.key=/home/slonnik/ssl/slonnik.site.key \
+	--from-file=docker.slonnik.site.crt=/home/slonnik/ssl/slonnik.site.crt \
+	--from-file=docker.slonnik.site.key=/home/slonnik/ssl/slonnik.site.key \
+	--from-file=ca.crt=/home/slonnik/ssl/ca.crt \
